b3ae27900bbad55ffd2a8d45fab60e6c10003a6f
Mattias Ståhlberg <46970089+ai7900@users.noreply.github.com>
2020-02-28 14:53:19 +0100

    New Folder - Basic Skeleton
    Here is the basic skeleton all assignments should follow.

---
+++ Fysik - Programmeringsuppgifter/Basic skeleton/Game1/Game1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game1
+{
+    public partial class Form1 : Form
+    {
+
+        Game1 myGame;
+        public Form1(Game1 myGame)
+        {
+            this.myGame = myGame;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float myValue;
+            if (float.TryParse(textBox1.Text, out myValue))
+                myGame.SetValue(myValue);
+            else
+                MessageBox.Show("ej giltigtflyttalsformat");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
